77631ff9a98a2f80d352b7e4cc7b18f1edfc3405
4925k <dibekpoudyal@gmail.com>
2022-02-03 13:45:36 +0545

    created a config file, added logging to file

--- logging/logging.go
+++ logging/logging.go
@@ -5,11 +5,11 @@ import (
 	"os"
 )
 
-func New() *log.Logger {
+func New(filePath string) *log.Logger {
 	logger := log.New(os.Stdout, "", log.LstdFlags|log.Lshortfile)
 
 	// If the file doesn't exist, create it or append to the file
-	file, err := os.OpenFile("./logging/microservice.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	file, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 	if err != nil {
 		log.Fatal(err)
 	}
--- main.go
+++ main.go
@@ -1,21 +1,19 @@
 package main
 
 import (
+	"microservice/config"
 	"microservice/handler"
 	"microservice/logging"
 	"microservice/server"
 	"net/http"
 )
 
-const (
-	addr = ":8080"
-	crt  = "./certs/server.crt"
-	key  = "./certs/server.key"
-)
-
 func main() {
+	// read config file
+	cfg := config.Make()
+
 	// set up logging with standard flags and file name
-	logger := logging.New()
+	logger := logging.New(cfg.Log)
 
 	// creating a router to route the incoming requets
 	mux := http.NewServeMux()
@@ -26,7 +24,7 @@ func main() {
 	// set up routes
 	handler.Routes(mux)
 
-	srv := server.New(mux, addr)
-	logger.Printf("[INFO] starting server at %s\n", addr)
-	logger.Fatalf("[FATAL] server failed to start: %v", srv.ListenAndServeTLS(crt, key))
+	srv := server.New(mux, cfg.Addr)
+	logger.Printf("[INFO] starting server at %s\n", cfg.Addr)
+	logger.Fatalf("[FATAL] server failed to start: %v", srv.ListenAndServeTLS(cfg.Crt, cfg.Key))
 }
