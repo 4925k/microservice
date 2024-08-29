ce92a09c2f82f92007f27eeb46c8a80dd5f75f70
dibek <dibekpoudyal@gmail.com>
2024-08-29 14:49:05 +0545

    add json data handler and makefile

--- broker/cmd/api/handler.go
+++ broker/cmd/api/handler.go
@@ -1,15 +1,8 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 )
-
-type brokerResponse struct {
-	Error   bool   `json:"error,omitempty"`
-	Message string `json:"message,omitempty"`
-	Data    any    `json:"data,omitempty"`
-}
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 	data := &brokerResponse{
@@ -17,13 +10,5 @@ func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 		Message: "OK",
 	}
 
-	out, err := json.Marshal(data)
-	if err != nil {
-		// handle error
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusAccepted)
-
-	_, _ = w.Write(out)
+	_ = app.writeJSON(w, r, http.StatusOK, data)
 }
