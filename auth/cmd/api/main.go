af77836bb822447a4aabdb1280393f5b7ee2daa1
dibek <dibekpoudyal@gmail.com>
2024-08-30 15:27:37 +0545

    added auth service framework

---
+++ auth/cmd/api/main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/microservice/auth/data"
+	"log"
+	"net/http"
+	"time"
+
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
+const (
+	dsn     = "host=localhost user=psotgres password=password dbname=user sslmode=disable"
+	webPort = "9997"
+)
+
+var (
+	dbCount int64
+)
+
+type Config struct {
+	DB     *sql.DB
+	Models data.Models
+}
+
+func main() {
+	log.Printf("Starting auth api at %s\n", webPort)
+
+	// connect to DB
+	conn := connectToDB()
+	if conn == nil {
+		log.Fatal("Unable to connect to database")
+	}
+
+	// set up config
+	app := Config{
+		DB:     conn,
+		Models: data.New(conn),
+	}
+
+	// server config
+	srv := http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	// start server
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+// openDB helps to get a db connection using the given dsn
+func openDB(dsn string) (*sql.DB, error) {
+	// open connection
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	// test connection
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+// connectToDB helps to connect to database
+func connectToDB() *sql.DB {
+	for {
+		db, err := openDB(dsn)
+		if err != nil {
+			log.Printf("Error connecting to database: %v\n", err)
+			dbCount++
+		} else {
+			return db
+		}
+
+		if dbCount > 10 {
+			log.Printf("Cannot connect to database after 10 retries\n")
+			return nil
+		}
+
+		log.Printf("Waiting for database to become available...\n")
+		time.Sleep(5 * time.Second)
+		continue
+	}
+}
---
+++ auth/cmd/api/routes.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+	"net/http"
+)
+
+func (app *Config) routes() http.Handler {
+	mux := chi.NewRouter()
+
+	// allow all for cors
+	mux.Use(cors.AllowAll().Handler)
+
+	// health check endpoint
+	mux.Use(middleware.Heartbeat("/ping"))
+
+	return mux
+}
