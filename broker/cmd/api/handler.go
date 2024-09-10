51b02a9b1129eb951fb13490810cbf47519c4037
dibek <dibekpoudyal@gmail.com>
2024-09-10 17:37:17 +0545

    add handler for mail service

--- broker/cmd/api/handler.go
+++ broker/cmd/api/handler.go
@@ -12,6 +12,14 @@ type RequestPayload struct {
 	Action string      `json:"action,omitempty"`
 	Auth   AuthPayload `json:"auth,omitempty"`
 	Log    LogPayload  `json:"log,omitempty"`
+	Mail   MailPayload `json:"mail,omitempty"`
+}
+
+type MailPayload struct {
+	From    string `json:"from,omitempty"`
+	To      string `json:"to,omitempty"`
+	Subject string `json:"subject,omitempty"`
+	Message string `json:"message,omitempty"`
 }
 
 // AuthPayload is the type for the auth
@@ -52,6 +60,9 @@ func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
 		return
 	case "logger":
 		app.log(w, req.Log)
+		return
+	case "mail":
+		app.sendMail(w, req.Mail)
 		return
 	default:
 		_ = app.writeError(w, errors.New("action not supported"), http.StatusBadRequest)
@@ -182,4 +193,43 @@ func (app *Config) logRequest(name, data string) error {
 	}
 
 	return nil
+}
+
+// mailRequest will send an email
+func (app *Config) sendMail(w http.ResponseWriter, data MailPayload) {
+	// parse data
+	jsonData, _ := json.Marshal(data)
+
+	// call mail service
+	mailServiceURL := "http://mail/send"
+
+	// create request
+	req, err := http.NewRequest("POST", mailServiceURL, bytes.NewBuffer(jsonData))
+	if err != nil {
+		_ = app.writeError(w, err)
+		return
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		_ = app.writeError(w, err)
+		return
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusAccepted {
+		_ = app.writeError(w, errors.New("error calling mail service"))
+		return
+	}
+
+	// return response
+	var payload serviceResponse
+	payload.Error = false
+	payload.Message = "Message sent to " + data.To
+
+	_ = app.writeJSON(w, http.StatusAccepted, payload)
 }
